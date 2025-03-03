0e68d7467f479b34e1e5e42d899f7f93bffea906
HowieTKL <10776992+HowieTKL@users.noreply.github.com>
2025-03-03 09:45:45 -0500

    Not Found

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -25,7 +25,7 @@ public class Main {
        if ("/".equals(requestLineParts[1])) {
          out.print("HTTP/1.1 200 OK\r\n\r\n");
        } else {
-         out.print("HTTP/1.1 404 OK\r\n\r\n");
+         out.print("HTTP/1.1 404 Not Found\r\n\r\n");
        }
        out.flush();
 
